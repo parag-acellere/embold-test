e4523fa6807ca409999d841c0fea3de6d3b73d1f
parag-acellere <parag.dalal@embold.io>
2022-03-14 16:40:16 +0530

    EMB-2

---
+++ CipherExample.java
@@ -0,0 +1,10 @@
+
+class CipherExample {
+public void foo() {
+// Non-compliant code
+try {
+Cipher c = Cipher.getInstance("DES");
+// Or DESede, RC2, RC4 as these are known to be vulnerable
+} catch (NoSuchAlgorithmException|NoSuchPaddingException e) {
+logger.error("Invalid algorithm",e);
+}
